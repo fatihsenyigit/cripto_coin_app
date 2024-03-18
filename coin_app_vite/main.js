6e1a62d0ff6fcac717b9c0975900d5edee7b383c
fatih <fsenyigit8@gmail.com>
2024-03-18 20:55:10 +0300

    form addeventlistener

--- coin_app_vite/main.js
+++ coin_app_vite/main.js
@@ -1,7 +1,18 @@
 import "./scss/style.scss"
 
-document.querySelector('#app').innerHTML = `
-  
+// selectors
+const form = document.querySelector('header form')
 
-`
+// events
+form.addEventListener('submit', (e)=>{
+  e.preventDefault()
+  getCoinData()
+  e.target.reset() // formu silme davranisini aktif hale getirir
+})
 
+const getCoinData = () => {
+  const input = document.querySelector('header form input').value
+  if(!input.trim()) {
+    alert('input must be entered')
+  }
+}
